c43823cab6e2e81822a2171cffb62e7b756f324f
Danny Guinther <dannyguinther@gmail.com>
2022-07-12 10:02:29 -0400

    Require sudo to install aws cli

--- src/scripts/003000_aws_cli.sh
+++ src/scripts/003000_aws_cli.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root or with sudo"
+  exit
+fi
+
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 ./aws/install
 rm -r awscliv2.zip aws
-
