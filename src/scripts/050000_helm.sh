df9109248aa2d4935fcdd67d83dcc61cb07005e1
Danny Guinther <dannyguinther@gmail.com>
2022-07-12 10:04:06 -0400

    Adjust helm script to require sudo

--- src/scripts/050000_helm.sh
+++ src/scripts/050000_helm.sh
@@ -1,5 +1,14 @@
-curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+#!/bin/bash
+
+set -e
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root or with sudo"
+  exit
+fi
+
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
 apt-get install apt-transport-https --yes
-echo "deb https://baltocdn.com/helm/stable/debian/ all main" | tee /etc/apt/sources.list.d/helm-stable-debian.list
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 apt-get update
 apt-get install helm
