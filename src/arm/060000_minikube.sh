162a41afc6d69b08587baef4c0bd40b2a641a00b
Danny Guinther <dannyguinther@gmail.com>
2023-01-17 09:43:58 -0500

    Add script to install minikube on arm

---
+++ src/arm/060000_minikube.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-arm64
+sudo install minikube-linux-arm64 /usr/local/bin/minikube
+rm minikube-linux-arm64
